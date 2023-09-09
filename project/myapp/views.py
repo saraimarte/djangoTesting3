1bf59539c7fbaec52da251ed5bc66c95f2117b76
Sarai Marte <smartepujols@gmail.com>
2023-09-09 13:28:39 -0400

    090923 users can add files and then download them!

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -3,7 +3,26 @@ from .models import TodoItem, Student
 # Create your views here.
 
 def home(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        grade = request.POST['grade']
+        essay = request.POST['essay']
+
+        new_student = Student(name = name, grade = grade, essay = essay)
+        new_student.save()
+
+        studentItems = Student.objects.all()
+        context = {"students": studentItems}
+        return render(request, "home.html", context)
+    else:
+        return render(request, "home.html")
+
+
+
+'''
+def home(request):
     items = TodoItem.objects.all()
     studentItems = Student.objects.all()
     context = {"todos":items, "students":studentItems}
     return render(request, "home.html", context)
+'''
